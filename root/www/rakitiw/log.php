62645e8ab71b6b059a1831129e0578ec46a84cad
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2024-03-24 03:16:30 +0700

    Cek Log

--- root/www/rakitiw/log.php
+++ root/www/rakitiw/log.php
@@ -1,7 +1,12 @@
 <?php
 $log_file = '/var/log/modemngentod.log';
-$log_lines = file($log_file);
-foreach ($log_lines as $line) {
-    echo nl2br($line);
+
+if (!file_exists($log_file)) {
+    echo "File Log belum ada.";
+} else {
+    $log_lines = file($log_file);
+    foreach ($log_lines as $line) {
+        echo nl2br($line);
+    }
 }
 ?>
