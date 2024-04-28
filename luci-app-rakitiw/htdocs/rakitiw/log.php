7767cb94bc235930f386533a6ea52ebb606e2b13
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2024-04-28 19:48:52 +0700

    fix

--- luci-app-rakitiw/htdocs/rakitiw/log.php
+++ luci-app-rakitiw/htdocs/rakitiw/log.php
@@ -7,6 +7,10 @@ if (!file_exists($log_file)) {
     $log_lines = file($log_file);
     foreach ($log_lines as $line) {
         echo nl2br($line);
+        if (strpos($line, "Setup Done | Modem Rakitiw Berhasil Di Install") !== false) {
+            // Jika iya, kirim sinyal ke JavaScript untuk melakukan redirect
+            echo "<script>window.location.href = 'index.php';</script>";
+        }
     }
 }
 ?>
